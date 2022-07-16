3453fee02ec0f3204220397fdc616e30eb2c1cba
Tripp Barker <colemanbarker@gmail.com>
2022-07-15 23:46:18 -0400

    Added functionality to copy button. Added a simple Ceasar Cipher function to test site while I work on the actual Enigma Machine logic.

--- enigma.js
+++ enigma.js
@@ -6,7 +6,100 @@ const orgMssg = document.querySelector('#OriginalMessage');
 const cryptMssg = document.querySelector('#CryptedMessage');
 
 function cryptMessage(e){
-    cryptMssg.textContent = orgMssg.value;
+    cryptMssg.textContent = orgMssg.value.toUpperCase();
+}
+
+function simpleCaesarCipher(e){
+    var message = "";
+    for (var i = 0; i < orgMssg.value.length; i++){
+        switch (orgMssg.value.charAt(i).toUpperCase()){
+            case "A":
+                message += "Z";
+                break;
+            case "B":
+                message += "Y";
+                break;
+            case "C":
+                message += "X";
+                break;
+            case "D":
+                message += "W";
+                break;
+            case "E":
+                message += "V";
+                break;
+            case "F":
+                message += "U";
+                break;
+            case "G":
+                message += "T";
+                break;
+            case "H":
+                message += "S";
+                break;
+            case "I":
+                message += "R";
+                break;
+            case "J":
+                message += "Q";
+                break;
+            case "K":
+                message += "P";
+                break;
+            case "L":
+                message += "O";
+                break;
+            case "M":
+                message += "N";
+                break;
+            case "N":
+                message += "M";
+                break;
+            case "O":
+                message += "L";
+                break;
+            case "P":
+                message += "K";
+                break;
+            case "Q":
+                message += "J";
+                break;
+            case "R":
+                message += "I";
+                break;
+            case "S":
+                message += "H";
+                break;
+            case "T":
+                message += "G";
+                break;
+            case "U":
+                message += "F";
+                break;
+            case "V":
+                message += "E";
+                break;
+            case "W":
+                message += "D";
+                break;
+            case "X":
+                message += "C";
+                break;
+            case "Y":
+                message += "B";
+                break;
+            case "Z":
+                message += "A";
+                break;
+            default:
+                continue;
+        }
+    }
+    cryptMssg.textContent = message;
+}
+
+function copyMessage(e){
+    navigator.clipboard.writeText(cryptMssg.value);
 }
 
 function clearMessage(e){
@@ -26,7 +119,8 @@ function typedLetter(e){
     }
 }
 
-crptKey.addEventListener('click', cryptMessage);
+crptKey.addEventListener('click', simpleCaesarCipher);
+copyKey.addEventListener('click', copyMessage);
 clrKey.addEventListener('click', clearMessage);
 keys.forEach(button => button.addEventListener('click', typeLetter));
 window.addEventListener('keydown', typedLetter);
